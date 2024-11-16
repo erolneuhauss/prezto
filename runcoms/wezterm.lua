1c24e2be3da1d0cdb4c5d451ebdad5ff849b9ebf
eneuhauss <erol.neuhauss@gmx.de>
2024-11-16 16:32:06 +0100

    Configure WezTerm

---
+++ runcoms/wezterm.lua
@@ -0,0 +1,6 @@
+local wezterm = require 'wezterm'
+local config = {}
+config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Medium'})
+config.font_size = 24
+config.color_scheme = 'Tokyo Night'
+return config
