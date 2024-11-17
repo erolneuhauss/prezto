66d51d6c8b05da53cb659fe34a9e97b2139c72cc
eneuhauss <erol.neuhauss@gmx.de>
2024-11-17 17:51:18 +0100

    extend wezterm word boundary

--- runcoms/wezterm.lua
+++ runcoms/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require 'wezterm'
 local config = {}
+config.color_scheme = 'Tokyo Night'
 config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Medium'})
 config.font_size = 24
-config.color_scheme = 'Tokyo Night'
+config.selection_word_boundary = ' \t\n{}[]()"\'`.,;:'
 return config
