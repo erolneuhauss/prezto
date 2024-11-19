eb1b6b209fe4b22db479b757b9f54781cfa26dc6
eneuhauss <erol.neuhauss@gmx.de>
2024-11-19 20:26:41 +0100

    let .wezterm.lua be tracked by chezmoi

--- runcoms/wezterm.lua
+++
@@ -1,7 +0,0 @@
-local wezterm = require 'wezterm'
-local config = {}
-config.color_scheme = 'Tokyo Night'
-config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Medium'})
-config.font_size = 24
-config.selection_word_boundary = ' \t\n{}[]()"\'`.,;:'
-return config
